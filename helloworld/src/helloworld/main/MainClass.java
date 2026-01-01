0187c5bbc21b6a74d9777b4a03d61c59fa65574f
rajesh180179 <raj.chni1979@gmail.com>
2026-01-01 19:42:48 +0530

    changed the  MathOpts methods as public and access the class from the main method

--- helloworld/src/helloworld/main/MainClass.java
+++ helloworld/src/helloworld/main/MainClass.java
@@ -1,10 +1,17 @@
 package helloworld.main;
 
+import helloworld.pack1.MathOprs;
+
 public class MainClass {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("New message added");
+		
+		
+		MathOprs oprs = new MathOprs();
+		System.out.println(oprs.sum(10, 20)); 
+		
 	}
 
 }
--- helloworld/src/helloworld/pack1/MathOprs.java
+++ helloworld/src/helloworld/pack1/MathOprs.java
@@ -2,7 +2,7 @@ package helloworld.pack1;
 
 public class MathOprs {
 
-	int sum (int a, int b) {
+	public int sum (int a, int b) {
 		return a+b;
 	}
 }
